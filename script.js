3207826899ce2d8e8b72b26945beb834b0a07408
Achal <achaldev3@gmail.com>
2024-01-21 01:36:24 +0530

    add all functionality

--- script.js
+++ script.js
@@ -1,37 +1,72 @@
 const BASE_URL = "https://api.github.com/users/";
 
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   async () => {
-//     const response = await fetch(`${BASE_URL}github`, {
-//       headers: {
-//         Authorization: "token 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",
-//       },
-//     });
-//     const data = await response.json();
-//     displayUser(data);
-//   },
-//   false
-// );
+let repoCount = 0;
+let username = "github";
+let perPage = 10;
+let currentPage = 1;
+
+var activities = document.getElementById("repository-count");
+
+activities.addEventListener("change", function () {
+  switch (activities.value) {
+    case "10":
+      perPage = 10;
+      fetchRepo();
+      break;
+    case "25":
+      perPage = 25;
+      fetchRepo();
+      break;
+    case "50":
+      perPage = 50;
+      fetchRepo();
+      break;
+    case "75":
+      perPage = 75;
+      fetchRepo();
+      break;
+    case "100":
+      perPage = 100;
+      fetchRepo();
+      break;
+    default:
+      break;
+  }
+});
+
+// Initial Fetchng
+
+document.addEventListener(
+  "DOMContentLoaded",
+  async () => {
+    document.querySelector(".overlay").style.display = "flex";
+    const response = await fetch(`${BASE_URL}${username}`);
+    const data = await response.json();
+    repoCount = data.public_repos;
+    displayUser(data);
+    fetchRepo();
+    document.querySelector(".overlay").style.display = "none";
+  },
+  false
+);
+
+// Fetching onSubmit
 
 const onSubmit = async (event) => {
   event.preventDefault();
   document.querySelector(".overlay").style.display = "flex";
-  let username = document.getElementById("username").value;
+  username = document.getElementById("username").value;
 
-  const response = await fetch(`${BASE_URL}${username}`, {
-    headers: {
-      Authorization: "token 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",
-    },
-  });
+  const response = await fetch(`${BASE_URL}${username}`);
 
   if (response.ok) {
     document
       .querySelector(".search-bar")
       .style.setProperty("--error-disp", "none");
     const data = await response.json();
+    repoCount = data.public_repos;
     displayUser(data);
-    displayRes(username);
+    fetchRepo();
   } else {
     document
       .querySelector(".search-bar")
@@ -39,6 +74,8 @@ const onSubmit = async (event) => {
   }
   document.querySelector(".overlay").style.display = "none";
 };
+
+// Display user details
 
 const displayUser = (data) => {
   document
@@ -68,206 +105,138 @@ const displayUser = (data) => {
 
   if (data.blog) {
     document.querySelector(".blog").style.display = "flex";
-    document
-      .querySelector(".blog a")
-      .setAttribute("href", `https://${data.blog}`);
+    document.querySelector(".blog a").setAttribute("href", data.blog);
     document.querySelector(".blog a").innerText = data.blog;
   } else {
     document.querySelector(".blog").style.display = "none";
   }
 };
 
-const displayRes = (user) => {
-  const username = user;
-  let perPage = 10;
-  let currentPage = 1;
+// Fetching rhe repository
 
-  const fetchRepo = async (action) => {
-    if (action === "prev" && currentPage > 1) {
-      currentPage--;
-    } else if (action === "next") {
-      currentPage++;
+const fetchRepo = async (action) => {
+  const repositoriesClass = document.querySelector(".repositories");
+  repositoriesClass.innerHTML = "";
+  document.querySelector(".repo-overlay").style.display = "flex";
+  document.querySelector(".pagination").style.display = "none";
+  if (action === "prev" && currentPage > 1) {
+    currentPage--;
+  } else if (action === "next") {
+    currentPage++;
+  }
+
+  const response = await fetch(
+    `${BASE_URL}${username}/repos?per_page=${perPage}&page=${currentPage}`,
+    {
+      headers: {
+        Accept: "application/vnd.github.mercy-preview+json",
+      },
     }
-
-    const response = await fetch(
-      `https://api.github.com/users/${username}/repos?per_page=${perPage}&page=${currentPage}`,
-      {
-        headers: {
-          Accept: "application/vnd.github.mercy-preview+json",
-          Authorization: "token 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",
-        },
-      }
-    );
-    const data = await response.json();
-    displayRepo(data);
-    updatePagination(data.length);
-  };
-
-  const displayRepo = (repositories) => {
-    const repositoriesClass = document.querySelector(".repositories");
-    repositoriesClass.innerHTML = "";
-
-    repositories.forEach((repo) => {
-      const repoElement = document.createElement("div");
-      const heading = document.createElement("h1");
-      const desc = document.createElement("p");
-      const topicContainer = document.createElement("div");
-      heading.textContent = repo.name;
-      desc.textContent = repo.description;
-
-      repo.topics.forEach((item) => {
-        const topic = document.createElement("span");
-        topic.textContent = item;
-        topicContainer.appendChild(topic);
-      });
-
-      repoElement.appendChild(heading);
-      repoElement.appendChild(desc);
-      repoElement.appendChild(topicContainer);
-      repositoriesClass.appendChild(repoElement);
-    });
-  };
-
-  const updatePagination = (repoCount) => {
-    const paginationNumbersDiv = document.querySelector(".pagination-numbers");
-    paginationNumbersDiv.innerHTML = "";
-
-    const totalPages = Math.ceil(repoCount / perPage); // 9
-    const maxButtons = 5;
-    const halfMaxButtons = Math.floor(maxButtons / 2); // 2
-
-    let startPage = Math.max(1, currentPage - halfMaxButtons); // 1
-    let endPage = Math.min(totalPages, startPage + maxButtons - 1); // 5
-
-    if (endPage - startPage < maxButtons - 1) {
-      // false
-      startPage = Math.max(1, endPage - maxButtons + 1);
-    }
-
-    console.log(startPage, endPage);
-
-    // Previous button
-    const previousButton = createPaginationButton("Previous", () => {
-      if (currentPage > 1) {
-        currentPage--;
-        fetchRepo();
-      }
-    });
-    paginationNumbersDiv.appendChild(previousButton);
-
-    // Numbered buttons
-    for (let i = startPage; i <= endPage; i++) {
-      const pageNumberButton = createPaginationButton(i, () => {
-        currentPage = i;
-        fetchRepo();
-      });
-      paginationNumbersDiv.appendChild(pageNumberButton);
-    }
-
-    // Next button
-    const nextButton = createPaginationButton("Next", () => {
-      if (currentPage < totalPages) {
-        currentPage++;
-        fetchRepo();
-      }
-    });
-    paginationNumbersDiv.appendChild(nextButton);
-
-    function createPaginationButton(label, onClick) {
-      const button = document.createElement("button");
-      button.textContent = label;
-      button.onclick = onClick;
-      return button;
-    }
-
-    // const previousButton = document.createElement("button");
-    // previousButton.textContent = "Previous";
-    // previousButton.onclick = () => fetchRepo("prev");
-    // paginationNumbersDiv.appendChild(previousButton);
-
-    // for (let i = 1; i <= currentPage + 1; i++) {
-    //   const pageNumberButton = document.createElement("button");
-    //   pageNumberButton.textContent = i;
-    //   pageNumberButton.onclick = () => {
-    //     currentPage = i;
-    //     fetchRepo();
-    //   };
-    //   paginationNumbersDiv.appendChild(pageNumberButton);
-    // }
-
-    // const nextButton = document.createElement("button");
-    // nextButton.textContent = "Next";
-    // nextButton.onclick = () => fetchRepo("next");
-    // paginationNumbersDiv.appendChild(nextButton);
-  };
-
-  fetchRepo();
+  );
+  const data = await response.json();
+  displayRepo(data);
+  updatePagination();
+  document.querySelector(".repo-overlay").style.display = "none";
+  document.querySelector(".pagination").style.display = "flex";
 };
 
-// const username = "achal-00";
-// const perPage = 10;
-// let currentPage = 1;
+// Display the repositories
 
-// function fetchPage(action) {
-//   if (action === "prev" && currentPage > 1) {
-//     currentPage--;
-//   } else if (action === "next") {
-//     currentPage++;
-//   }
+const displayRepo = (repositories) => {
+  const repositoriesClass = document.querySelector(".repositories");
+  repositoriesClass.innerHTML = "";
 
-//   const url = `https://api.github.com/users/${username}/repos?per_page=${perPage}&page=${currentPage}`;
+  repositories.forEach((repo) => {
+    const repoElement = document.createElement("div");
+    const heading = document.createElement("h1");
+    const desc = document.createElement("p");
+    const topicContainer = document.createElement("div");
+    const extraInfo = document.createElement("div");
+    const starCount = document.createElement("div");
+    const forkCount = document.createElement("div");
+    const starIcon = document.createElement("img");
+    const starNo = document.createElement("p");
+    const forkIcon = document.createElement("img");
+    const forkNo = document.createElement("p");
+    extraInfo.className = "extra-info";
+    heading.textContent = repo.name;
+    desc.textContent = repo.description;
+    starIcon.src = "./assets/star-icon.svg";
+    starNo.textContent = repo.stargazers_count;
+    forkIcon.src = "./assets/fork-icon.svg";
+    forkNo.textContent = repo.forks_count;
 
-//   fetch(url, {
-//     headers: {
-//       Accept: "application/vnd.github.v3+json",
-//       Authorization: "token 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",
-//     },
-//   })
-//     .then((response) => response.json())
-//     .then((repositories) => {
-//       console.log(repositories);
-//       displayRepositories(repositories);
-//       updatePagination();
-//     })
-//     .catch((error) => console.error("Error fetching repositories:", error));
-// }
+    repo.topics.forEach((item) => {
+      const topic = document.createElement("span");
+      topic.textContent = item;
+      topicContainer.appendChild(topic);
+    });
 
-// function updatePagination() {
-//   const paginationNumbersDiv = document.getElementById("paginationNumbers");
-//   paginationNumbersDiv.innerHTML = "";
+    starCount.appendChild(starIcon);
+    starCount.appendChild(starNo);
+    forkCount.appendChild(forkIcon);
+    forkCount.appendChild(forkNo);
 
-//   // Add Previous button
-//   const previousButton = document.createElement("button");
-//   previousButton.textContent = "Previous";
-//   previousButton.onclick = () => fetchPage("prev");
-//   paginationNumbersDiv.appendChild(previousButton);
+    extraInfo.appendChild(starCount);
+    extraInfo.appendChild(forkCount);
 
-//   // Add numbered buttons
-//   for (let i = 1; i <= currentPage + 2; i++) {
-//     const pageNumberButton = document.createElement("button");
-//     pageNumberButton.textContent = i;
-//     pageNumberButton.onclick = () => fetchPage(i);
-//     paginationNumbersDiv.appendChild(pageNumberButton);
-//   }
+    repoElement.appendChild(heading);
+    repoElement.appendChild(desc);
+    repoElement.appendChild(topicContainer);
+    repoElement.appendChild(extraInfo);
+    repositoriesClass.appendChild(repoElement);
 
-//   // Add Next button
-//   const nextButton = document.createElement("button");
-//   nextButton.textContent = "Next";
-//   nextButton.onclick = () => fetchPage("next");
-//   paginationNumbersDiv.appendChild(nextButton);
-// }
+    repoElement.addEventListener("click", () => {
+      window.open(repo.html_url);
+    });
+  });
+};
 
-// function displayRepositories(repositories) {
-//   const repositoriesDiv = document.getElementById("repositories");
+// Update pagination
 
-//   repositoriesDiv.innerHTML = ""; // Clear previous content
+const updatePagination = () => {
+  const paginationNumbersDiv = document.querySelector(".pagination-numbers");
+  paginationNumbersDiv.innerHTML = "";
 
-//   repositories.forEach((repo) => {
-//     const repoElement = document.createElement("div");
-//     repoElement.textContent = repo.name;
-//     repositoriesDiv.appendChild(repoElement);
-//   });
-// }
+  const totalPages = Math.ceil(repoCount / perPage);
+  const maxButtons = 5;
+  const halfMaxButtons = Math.floor(maxButtons / 2);
 
-// // Initial fetch
-// fetchPage();
+  let startPage = Math.max(1, currentPage - halfMaxButtons);
+  let endPage = Math.min(totalPages, startPage + maxButtons - 1);
+
+  if (endPage - startPage < maxButtons - 1) {
+    startPage = Math.max(1, endPage - maxButtons + 1);
+  }
+
+  const previousButton = createPaginationButton("Previous", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      fetchRepo();
+    }
+  });
+  paginationNumbersDiv.appendChild(previousButton);
+
+  for (let i = startPage; i <= endPage; i++) {
+    const pageNumberButton = createPaginationButton(i, () => {
+      currentPage = i;
+      fetchRepo();
+    });
+    paginationNumbersDiv.appendChild(pageNumberButton);
+  }
+
+  const nextButton = createPaginationButton("Next", () => {
+    if (currentPage < totalPages) {
+      currentPage++;
+      fetchRepo();
+    }
+  });
+  paginationNumbersDiv.appendChild(nextButton);
+
+  function createPaginationButton(label, onClick) {
+    const button = document.createElement("button");
+    button.textContent = label;
+    button.onclick = onClick;
+    return button;
+  }
+};
